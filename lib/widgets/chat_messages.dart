9342780078f40533ecc48d27f500c83bf1b9a158
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2024-01-03 22:07:52 -0800

    added message widgets to chat screen. Added messages to firebase cloud storage

---
+++ lib/widgets/chat_messages.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChatMessages extends StatelessWidget {
+  const ChatMessages({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return const Center(
+      child: Text('No Messages found'),
+    );
+  }
+}
