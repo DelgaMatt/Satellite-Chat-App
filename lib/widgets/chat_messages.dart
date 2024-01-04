fc9096aca65908d877fd3027a3a8f2fc3c45563b
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2024-01-03 22:20:45 -0800

    added chat messages to screen via firebase

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,12 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
-  Widget build(BuildContext) {
-    return const Center(
-      child: Text('No Messages found'),
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      // setting up a listener to the chat collection in the database, notifying the app, triggering build function so we can update the UI
+      stream: FirebaseFirestore.instance
+        .collection('chat')
+        .orderBy('createdAt', descending: false)
+        .snapshots(),
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(child: Text('No messages found'));
+        }
+
+        if (chatSnapshots.hasError) {
+          return const Center(
+            child: Text('Something went wrong..'),
+          );
+        }
+
+        final loadedMessages = chatSnapshots.data!.docs;
+
+        return ListView.builder(
+              itemCount: loadedMessages.length,
+              itemBuilder: (ctx, index) =>
+              Text(loadedMessages[index].data()['text']),
+        );
+      },
     );
   }
 }
